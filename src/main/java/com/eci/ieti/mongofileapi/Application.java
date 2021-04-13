ce45c9ba162d62720e582e8b9a66f933876c1831
Ricar8o <andres.martinez-d@mail.escuelaing.edu.co>
2021-04-13 14:53:08 -0500

    remove some files & redirect

--- src/main/java/com/eci/ieti/mongofileapi/Application.java
+++ src/main/java/com/eci/ieti/mongofileapi/Application.java
@@ -8,11 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.gridfs.GridFsTemplate;
-
-import com.mongodb.client.gridfs.model.GridFSFile;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
@@ -30,7 +27,7 @@ public class Application implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        GridFSFile file = gridFsTemplate.findOne(new Query().addCriteria(Criteria.where("filename").is("testing.png")));
+        gridFsTemplate.delete(new Query());
         URL url = new URL("https://i.dailymail.co.uk/i/pix/tm/2007/07/lionking1807_468x325._to_468x312jpeg");
         gridFsTemplate.store(url.openStream(), "lion.jpeg",  "image/jpeg");
         logger.info("Image successfully uploaded to the database");
--- src/main/java/com/eci/ieti/mongofileapi/controller/RESTController.java
+++ src/main/java/com/eci/ieti/mongofileapi/controller/RESTController.java
@@ -1,37 +1,59 @@
 package com.eci.ieti.mongofileapi.controller;
 
-
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.gridfs.GridFsResource;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import com.eci.ieti.mongofileapi.data.model.Todo;
+import com.mongodb.client.gridfs.model.GridFSFile;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @RequestMapping("api")
 @RestController
 public class RESTController {
-
-
-   //TODO inject components (TodoRepository and GridFsTemplate)
+    
+    @Autowired
+    GridFsTemplate gridFsTemplate;
 
     @RequestMapping("/files/{filename}")
     public ResponseEntity<InputStreamResource> getFileByName(@PathVariable String filename) throws IOException {
 
-        //TODO implement method
-        return null;
+        GridFSFile file = gridFsTemplate.findOne(new Query().addCriteria(Criteria.where("filename").is(filename)));
+
+        if (file!=null){
+            GridFsResource resource = gridFsTemplate.getResource(file.getFilename());
+        return ResponseEntity.ok()
+            .contentType(MediaType.valueOf(resource.getContentType()))
+            .body(new InputStreamResource(resource.getInputStream()));
+        }else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
 
     }
 
     @CrossOrigin("*")
     @PostMapping("/files")
-    public String handleFileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) throws IOException {
+    public ResponseEntity<String> handleFileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) throws IOException {
+        String name  = URLEncoder.encode(file.getOriginalFilename(), StandardCharsets.UTF_8);
+        gridFsTemplate.store(file.getInputStream(), name, file.getContentType());
 
-        //TODO implement method
-        return null;
+        return ResponseEntity.created(null)
+        .contentType(MediaType.APPLICATION_JSON)
+        .body("redirect:/api/files/"+name);
     }
 
     @CrossOrigin("*")
